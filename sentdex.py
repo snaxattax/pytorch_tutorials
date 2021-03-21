a3407e6f628c5c6c491dac0d91fa093ed406eeb7
Ubuntu <tillman.jack@gmail.com>
2021-03-21 10:44:56 -0700

    updates with basic pytorch demos and start of sentex demos, updates gitignore to not commit gigs of cruft

---
+++ sentdex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# working on the neural networks from scratch series by sentdex
+# uses python ~3 and numpy to build neural networks and implement
+# forward and back prop, and the various supporting elements to
+# implement it all.
+
+# https://www.youtube.com/watch?v=Wo5dMEP_BbI
+# nnfs.io
+
+import sys
+import numpy as np
+import matplotlib
+
