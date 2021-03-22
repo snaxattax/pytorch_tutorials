2dab18934e459e7886229961425525c92224bf68
Ubuntu <tillman.jack@gmail.com>
2021-03-22 12:07:55 -0700

    updates from video 6

--- sentdex6.py
+++ sentdex6.py
@@ -7,6 +7,9 @@ layer_outputs = [   [4.8, 1.21, 2.385],
                     [8.9, -1.81, 0.2],
                     [1.41, 1.051, 0.026]]
 
+
+
+
 exp_values = np.exp(layer_outputs)
 
 print(exp_values)
